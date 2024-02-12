32f52373b59b096c69ae834eec92dfbdca2a492d
Akshitha123-24 <mengansaiakshitha03@gmail.com>
2024-02-12 11:45:48 +0530

    Edited Landing Page

--- src/AdminRegister.jsx
+++ src/AdminRegister.jsx
@@ -3,11 +3,17 @@ import './css/UserRegister.css';
 import logo from './images/logo.png';
 import CommonInput from './CommonInput';
 import CtaButton from './CtaButton';
+import { useNavigate } from 'react-router-dom';
 
 const inputField =['Email ID', 'Full Name', 'Password', 'College'];
 
 const AdminRegister = () => {
-
+    const navigate = useNavigate();
+    const handleRegister = () => {
+        // Perform registration logic here
+        // Once registration is successful, redirect to the admin dashboard
+        navigate('/');
+      };
  return (
     <div className="admin-register">
         <div className="logo">
@@ -16,11 +22,11 @@ const AdminRegister = () => {
         <div className="register-form">
             <h1 className="title-heading">Register</h1>
             <div className="input-fields">
-                {inputField.map((item) => (
-                    <CommonInput placeholderText={item} />
+                {inputField.map((item,index) => (
+                    <CommonInput key={index} placeholderText={item} />
                 ))}
             </div>
-            <CtaButton text="Register" />
+            <CtaButton text="Register" onClick={handleRegister} />
         </div>
     </div>
 );
